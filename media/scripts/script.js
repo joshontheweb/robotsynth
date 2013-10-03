41c23fb4936b5c83950f0ae3899c515750fd8776
Josh Nielsen <josh@joshontheweb.com>
2013-10-02 23:15:47 -0600

    initial commit of robot launch

---
+++ media/scripts/script.js
@@ -0,0 +1,58 @@
+(function() {
+
+  $('.email-input').keypress(function(e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      $('.submit').click();
+    }
+
+  });
+
+  $('.submit').click(function(e) {
+    e.preventDefault();
+
+    $('.email-signup').fadeOut('fast', function() {
+      var opts = {
+        lines: 17, // The number of lines to draw
+        length: 30, // The length of each line
+        width: 5, // The line thickness
+        radius: 30, // The radius of the inner circle
+        rotate: 0, // The rotation offset
+        color: '#fff', // #rgb or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: true, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: 'auto', // Top position relative to parent in px
+        left: 'auto' // Left position relative to parent in px
+      };
+      window.spinner = new Spinner(opts).spin();
+      window.$spinner = $(window.spinner.el);
+      $('.main-content').append(window.$spinner);
+      window.$spinner.fadeIn();
+
+      $.ajax({
+        type: 'POST',
+        url: '/signup',
+        data: {email: $('.email-input').val()},
+        success: function(data) {
+          $('.email-input').val('');
+          window.$spinner.fadeOut('fast', function() {
+            window.spinner.stop();
+            $('.main-content').append('Thanks! You have been sent a confirmation email.');
+          })
+        },
+        error: function(data) {
+          window.$spinner.fadeOut('fast', function() {
+            window.spinner.stop();
+          });
+          $('.email-signup').fadeIn('fast', function() {
+            $('.main-content').append('<span class="error">Oh Noes! Something went wrong. :/</span>');
+          });
+        }
+      })
+    });
+  });
+})();
---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var _ = require('underscore');
+var exec = require('child_process').exec
+var port = 2012;
+var mcapiKey = '33aec45bf6894883a6a1b921ba3bfe2f-us2';
+var soundKeepListId = 'edb3858885';
+var MailChimpAPI = require('mailchimp').MailChimpAPI;
+
+try { 
+    var mcapi = new MailChimpAPI(mcapiKey, { version : '1.3', secure : false });
+} catch (error) {
+    console.log('Error: ' + error);
+}
+
+var app = express.createServer();
+
+app.use(express.bodyParser());
+app.use('/media', express.static(__dirname + '/media'));
+app.set('views', __dirname + '/templates');
+
+app.register('.html', {
+  compile: function (str, options) {
+    var template = _.template(str);
+    return function (locals) {
+      return template(locals);
+    };
+  }
+});
+
+app.get('/', function(req, res) {
+  res.render('index.html', {});
+});
+
+app.post('/signup', function(req, res) {
+  mcapi.listSubscribe({apikey: mcapiKey, id: soundKeepListId, email_address: req.body.email}, function(success) {
+    if (success) {
+      res.send('Success', 200);
+    } else {
+      res.send('Error', 405);
+    }
+  });
+});
+
+var child = exec('cowsay Running on port: '+port, function(err, stdout, stderr) {
+  if (!err) {
+	console.log(stdout);
+  } else {
+  	console.log('Running on port: '+port);
+  }
+});
+
+app.listen(port);
+
