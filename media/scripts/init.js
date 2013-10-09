9ce3ac0b1e6faba34137691ac34ff705845a2a6e
Josh Nielsen <josh@joshontheweb.com>
2013-10-09 17:32:52 -0600

    finished styling more info and adding web audio context check that works for safari

--- media/scripts/init.js
+++ media/scripts/init.js
@@ -1,5 +1,14 @@
 (function() {
   if (window.AudioContext) {
+
+    try {
+      // safari has an audio context but breaks on setting detune as of version 6.0.5
+      var osc = window.AudioContext.createOscillator();
+      osc.detune.value = 5;
+    } catch (err) {
+      $('.no-audio').fadeIn();
+    }
+    
     window.router = new bs.routers.Main();
     Backbone.history.start({pushState: true});
   } else {
